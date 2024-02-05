36203d7ee747cc418de2a7b5a686cbe0c78e326c
Sylwia Dobkowska <sylvia.dobkowska@gmail.com>
2024-02-05 17:41:11 +0100

    fixed using button in functions

--- js/script.js
+++ js/script.js
@@ -7,24 +7,25 @@
         const photo = document.getElementById("photo");
         photo.style.display = photo.style.display === "none" ? "block" : "none";
     }
-    
-    {
-        let button = document.querySelector(".button");
-    const changeButtonInnerText = () => {
+
+    const changeButtonInnerText = (button) => {
         togglePhoto();
 
         button.innerText = button.innerText === "Pokaż zdjęcie" ? "Ukryj zdjęcie" : "Pokaż zdjęcie";
     }
 
     const init = () => {
-        button.addEventListener("click", changeButtonInnerText);
+        const button = document.querySelector(".button");
+        button.addEventListener("click", () => {
+            changeButtonInnerText(button);
+        });
 
         welcome();
 
     }
 
     init();
-    }
+
 
 
 
